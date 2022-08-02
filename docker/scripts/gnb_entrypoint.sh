0c751586d95dce185295ce5638cf552b32ed91d1
Rohan <rohan.kharade@openairinterface.org>
2022-08-02 16:41:05 +0000

    Resolve AMF ip address using FQDN

--- docker/scripts/gnb_entrypoint.sh
+++ docker/scripts/gnb_entrypoint.sh
@@ -18,6 +18,11 @@ if [[ -v USE_SA_NFAPI_VNF ]]; then cp $PREFIX/etc/gnb.sa.nfapi.vnf.conf $PREFIX/
 # Sometimes, the templates are not enough. We mount a conf file on $PREFIX/etc. It can be a template itself.
 if [[ -v USE_VOLUMED_CONF ]]; then cp $PREFIX/etc/mounted.conf $PREFIX/etc/gnb.conf; fi
 
+# Resolve AMF FQDN
+USE_FQDN=${USE_FQDN:-no}
+AMF_FQDN=${AMF_FQDN:-oai-amf-svc}
+if [[ ${USE_FQDN} == "yes" ]]; then AMF_IP_ADDRESS=(`getent hosts $AMF_FQDN | awk '{print $1}'`); fi
+
 # Only this template will be manipulated
 CONFIG_FILES=`ls $PREFIX/etc/gnb.conf || true`
 
