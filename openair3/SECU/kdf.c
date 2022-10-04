96edf5909805090ee37d1e9966e632e2802bdf93
mir <mikel.irazabal@openairinterface.com>
2022-10-04 19:12:33 +0200

    First commit to get rid of nettle: HMAC 256

--- openair3/SECU/kdf.c
+++ openair3/SECU/kdf.c
@@ -19,87 +19,57 @@
  *      contact@openairinterface.org
  */
 
+#include <assert.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 
-#include <nettle/hmac.h>
+#include <openssl/core_names.h>
+#include <openssl/hmac.h>
 
-#include "security_types.h"
-#include "secu_defs.h"
+#include "kdf.h"
 
-void kdf(const uint8_t *key, uint16_t key_len, uint8_t *s, uint16_t s_len, uint8_t *out,
-         uint16_t out_len)
+void kdf(const uint8_t key[32], byte_array_t data, size_t len, uint8_t out[len])
 {
-  struct hmac_sha256_ctx ctx;
+  assert(key != NULL);
+  assert(data.buf != NULL);
+  assert(data.len != 0);
+  assert(len != 0);
 
-  memset(&ctx, 0, sizeof(ctx));
+  OSSL_LIB_CTX * library_context = OSSL_LIB_CTX_new();
+  assert(library_context != NULL);
 
-  hmac_sha256_set_key(&ctx, key_len, key);
-  hmac_sha256_update(&ctx, s_len, s);
-  hmac_sha256_digest(&ctx, out_len, out);
+  // A property query used for selecting the MAC implementation.
+  const char *propq = NULL;
+  /* Fetch the HMAC implementation */
+  EVP_MAC* mac = EVP_MAC_fetch(library_context, "HMAC", propq);
+  assert(mac != NULL);
+
+  /* Create a context for the HMAC operation */
+  EVP_MAC_CTX* mctx = EVP_MAC_CTX_new(mac);
+  assert(mctx != NULL);
+
+  /* The underlying digest to be used */
+  OSSL_PARAM params[2] = {0};
+  char digest_name[] = "SHA2-256";
+  params[0] = OSSL_PARAM_construct_utf8_string(OSSL_MAC_PARAM_DIGEST, digest_name, sizeof(digest_name));
+  params[1] = OSSL_PARAM_construct_end();
+
+  /* Initialise the HMAC operation */
+  int rc = EVP_MAC_init(mctx, key, 32, params);
+  assert(rc == 1);
+
+  /* Make one or more calls to process the data to be authenticated */
+  rc = EVP_MAC_update(mctx, data.buf, data.len);
+  assert(rc == 1);
+
+  /* Make one call to the final to get the MAC */
+  rc = EVP_MAC_final(mctx, out, &len, len);
+  assert(rc == 1);
+
+  /* OpenSSL free functions will ignore NULL arguments */
+  EVP_MAC_CTX_free(mctx);
+  EVP_MAC_free(mac);
+  OSSL_LIB_CTX_free(library_context);
 }
 
-#ifndef NAS_UE
-int derive_keNB(const uint8_t kasme[32], const uint32_t nas_count, uint8_t *keNB)
-{
-  uint8_t s[7];
-
-  // FC
-  s[0] = FC_KENB;
-  // P0 = Uplink NAS count
-  s[1] = (nas_count & 0xff000000) >> 24;
-  s[2] = (nas_count & 0x00ff0000) >> 16;
-  s[3] = (nas_count & 0x0000ff00) >> 8;
-  s[4] = (nas_count & 0x000000ff);
-
-  // Length of NAS count
-  s[5] = 0x00;
-  s[6] = 0x04;
-
-  kdf(kasme, 32, s, 7, keNB, 32);
-
-  return 0;
-}
-#endif
-
-int derive_keNB_star(
-  const uint8_t *kenb_32,
-  const uint16_t pci,
-  const uint32_t earfcn_dl,
-  const bool     is_rel8_only,
-  uint8_t       *kenb_star)
-{
-  // see 33.401 section A.5 KeNB* derivation function
-  uint8_t                                 s[10] = {0};
-
-  // FC = 0x13
-  s[0] = FC_KENB_STAR;
-  // P0 = PCI (target physical cell id)
-  s[1] = (pci & 0x0000ff00) >> 8;
-  s[2] = (pci & 0x000000ff);
-  // L0 = length of PCI (i.e. 0x00 0x02)
-  s[3] = 0x00;
-  s[4] = 0x02;
-  // P1 = EARFCN-DL (target physical cell downlink frequency)
-  if (is_rel8_only) {
-    s[5] = (earfcn_dl & 0x0000ff00) >> 8;
-    s[6] = (earfcn_dl & 0x000000ff);
-	s[7] = 0x00;
-	s[8] = 0x02;
-	kdf (kenb_32, 32, s, 9, kenb_star, 32);
-  } else {
-	s[5] = (earfcn_dl & 0x00ff0000) >> 16;
-	s[6] = (earfcn_dl & 0x0000ff00) >> 8;
-	s[7] = (earfcn_dl & 0x000000ff);
-	s[8] = 0x00;
-	s[9] = 0x03;
-	kdf (kenb_32, 32, s, 10, kenb_star, 32);
-  }
-  // L1 length of EARFCN-DL (i.e. L1 = 0x00 0x02 if EARFCN-DL is between 0 and 65535, and L1 = 0x00 0x03 if EARFCN-DL is between 65536 and 262143)
-  // NOTE: The length of EARFCN-DL cannot be generally set to 3 bytes for backward compatibility reasons: A Rel-8
-  // entity (UE or eNB) would always assume an input parameter length of 2 bytes for the EARFCN-DL. This
-  // would lead to different derived keys if another entity assumed an input parameter length of 3 bytes for the
-  // EARFCN-DL.
-  return 0;
-}
