ccae22f648f6d36bf495c36ed70b10af1a15ca6f
yacine <lacksley.castell@gmail.com>
2022-10-06 15:15:25 +0200

    serdes to be tested

--- common/utils/websrv/frontend/src/app/charts/wf.dataset.ts
+++ common/utils/websrv/frontend/src/app/charts/wf.dataset.ts
@@ -2,35 +2,48 @@ import { ChartConfiguration } from "chart.js";
 
 export const WFDatasets: ChartConfiguration<'scatter'>['data']['datasets'] = [
   {
-    data: [],
-    label: 'WF1',
+    data: [800 * 100],
+    label: 'WFblue',
     pointRadius: 0.5,
     showLine: false,
     animation: false,
     fill: false,
     pointStyle: 'circle',
-    pointBackgroundColor: 'yellow',
-    backgroundColor: 'yellow',
+    pointBackgroundColor: 'blue',
+    backgroundColor: 'blue',
     borderWidth: 0,
-    pointBorderColor: 'yellow',
-    //    parsing: false,
+    pointBorderColor: 'blue',
+    //     parsing: false,
   },
   {
-    data: [],
-    label: 'WF2',
+    data: [800 * 100],
+    label: 'WFgreen',
     pointRadius: 0.5,
     showLine: false,
     animation: false,
     pointStyle: 'circle',
-    pointBackgroundColor: 'cyan',
-    backgroundColor: 'cyan',
+    pointBackgroundColor: 'green',
+    backgroundColor: 'green',
     borderWidth: 0,
-    pointBorderColor: 'cyan',
+    pointBorderColor: 'green',
     //      parsing: false,
   },
   {
-    data: [],
-    label: 'WF3',
+    data: [800 * 100],
+    label: 'WFyellow',
+    pointRadius: 0.5,
+    showLine: false,
+    animation: false,
+    pointStyle: 'circle',
+    pointBackgroundColor: 'yellow',
+    backgroundColor: 'yellow',
+    borderWidth: 0,
+    pointBorderColor: 'yellow',
+    //      parsing: false,
+  },
+  {
+    data: [800 * 100],
+    label: 'WFred',
     pointRadius: 0.5,
     showLine: false,
     animation: false,
@@ -39,12 +52,23 @@ export const WFDatasets: ChartConfiguration<'scatter'>['data']['datasets'] = [
     backgroundColor: 'red',
     borderWidth: 0,
     pointBorderColor: 'red',
-    //      parsing: false,
+    //     parsing: false,
   }
 ];
 
 export const WFOptions: ChartConfiguration<'scatter'>['options'] = {
   responsive: true,
   aspectRatio: 5,
+  scales: {
+    xAxes: {
+      min: 0,
+      max: 800,
+    },
+    yAxes: {
+      min: 0,
+      max: 100,
+      reverse: true,
+    }
+  },
   plugins: { legend: { display: true, labels: { boxWidth: 10, boxHeight: 10 } }, tooltip: { enabled: false, }, },
 }
