99c54f79f5c67745078cb31727f512e03fb58619
francescomani <email@francescomani.it>
2022-11-23 13:53:02 +0100

    fixes at UE for transform precoding

--- openair2/LAYER2/NR_MAC_UE/nr_ue_scheduler.c
+++ openair2/LAYER2/NR_MAC_UE/nr_ue_scheduler.c
@@ -181,7 +181,6 @@ fapi_nr_ul_config_request_t *get_ul_config_request(NR_UE_MAC_INST_t *mac, int sl
 
 void ul_layers_config(NR_UE_MAC_INST_t * mac, nfapi_nr_ue_pusch_pdu_t *pusch_config_pdu, dci_pdu_rel15_t *dci) {
 
-  NR_ServingCellConfigCommon_t *scc = mac->scc;
   NR_BWP_Id_t ul_bwp_id = mac->current_UL_BWP.bwp_id;
   NR_BWP_UplinkDedicated_t *ubwpd=NULL;
   NR_SRS_Config_t *srs_config = NULL;
@@ -201,16 +200,7 @@ void ul_layers_config(NR_UE_MAC_INST_t * mac, nfapi_nr_ue_pusch_pdu_t *pusch_con
      ubwpd->pusch_Config->choice.setup:
      NULL);
 
-  long	transformPrecoder;
-  if (pusch_Config && pusch_Config->transformPrecoder)
-    transformPrecoder = *pusch_Config->transformPrecoder;
-  else {
-    if(scc && scc->uplinkConfigCommon->initialUplinkBWP->rach_ConfigCommon->choice.setup->msg3_transformPrecoder)
-      transformPrecoder = NR_PUSCH_Config__transformPrecoder_enabled;
-    else
-      transformPrecoder = NR_PUSCH_Config__transformPrecoder_disabled;
-  }
-  pusch_config_pdu->transform_precoding = transformPrecoder;
+  int transformPrecoder = pusch_config_pdu->transform_precoding;
 
   /* PRECOD_NBR_LAYERS */
   if ((*pusch_Config->txConfig == NR_PUSCH_Config__txConfig_nonCodebook));
@@ -739,7 +729,6 @@ int nr_config_pusch_pdu(NR_UE_MAC_INST_t *mac,
       pusch_config_pdu->scid = dci->dmrs_sequence_initialization.val;
 
     /* TRANSFORM PRECODING ------------------------------------------------------------------------------------------*/
-
     if (pusch_config_pdu->transform_precoding == NR_PUSCH_Config__transformPrecoder_enabled) {
 
       pusch_config_pdu->num_dmrs_cdm_grps_no_data = 2;
@@ -749,15 +738,17 @@ int nr_config_pusch_pdu(NR_UE_MAC_INST_t *mac,
           NR_DMRS_ulconfig->transformPrecodingEnabled->nPUSCH_Identity != NULL)
         n_RS_Id = *NR_DMRS_ulconfig->transformPrecodingEnabled->nPUSCH_Identity;
       else
-        n_RS_Id = *mac->scc->physCellId;
+        n_RS_Id = mac->physCellId;
 
       // U as specified in section 6.4.1.1.1.2 in 38.211, if sequence hopping and group hopping are disabled
       pusch_config_pdu->dfts_ofdm.low_papr_group_number = n_RS_Id % 30;
 
       // V as specified in section 6.4.1.1.1.2 in 38.211 V = 0 if sequence hopping and group hopping are disabled
-      if ((NR_DMRS_ulconfig->transformPrecodingEnabled->sequenceGroupHopping == NULL) &&
-            (NR_DMRS_ulconfig->transformPrecodingEnabled->sequenceHopping == NULL))
-          pusch_config_pdu->dfts_ofdm.low_papr_sequence_number = 0;
+      if ((!NR_DMRS_ulconfig ||
+          !NR_DMRS_ulconfig->transformPrecodingEnabled ||
+          (!NR_DMRS_ulconfig->transformPrecodingEnabled->sequenceGroupHopping && !NR_DMRS_ulconfig->transformPrecodingEnabled->sequenceHopping)) &&
+          !initialUplinkBWP->pusch_ConfigCommon->choice.setup->groupHoppingEnabledTransformPrecoding)
+        pusch_config_pdu->dfts_ofdm.low_papr_sequence_number = 0;
       else
         AssertFatal(1==0,"SequenceGroupHopping or sequenceHopping are NOT Supported\n");
 
