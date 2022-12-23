b4b0f87e2aa46082c0c1781fb2b22eb1c1fa74ac
mir <mikel.irazabal@openairinterface.com>
2022-12-23 17:38:03 +0100

    Avoiding linking objects in CMake

--- openair3/SECU/kdf.h
+++ openair3/SECU/kdf.h
@@ -22,6 +22,8 @@
 #ifndef KEY_DERIVATION_FUNCTION_OSA_H
 #define KEY_DERIVATION_FUNCTION_OSA_H
 
+#include <stdint.h>
+#include <stdlib.h>
 #include "byte_array.h"
 
 void kdf(const uint8_t key[32], byte_array_t data, size_t len, uint8_t out[len]);
